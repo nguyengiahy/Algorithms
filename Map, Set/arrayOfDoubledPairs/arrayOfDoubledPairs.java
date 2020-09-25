2c5d1fe90a1f8a3061e838f08a36a3af56fda1cd
Hy Nguyen <nguyengiahy2911@gmail.com>
2020-09-25 22:51:45 +0700

    fix arrayOfDoublePairs

--- Map, Set/arrayOfDoubledPairs/arrayOfDoubledPairs.java
+++ Map, Set/arrayOfDoubledPairs/arrayOfDoubledPairs.java
@@ -1,26 +1,25 @@
 class Solution {
     public boolean canReorderDoubled(int[] A) {
-        HashMap<Integer, Integer> frequency = new HashMap<Integer, Integer>();
-        Arrays.sort(A); // sort array
+        Arrays.sort(A);
+        HashMap<Integer, Integer> frequency = new HashMap<>();  // frequency of occurrences
         
-        for (int i=0;i<A.length;i++)
-            frequency.put(A[i], frequency.getOrDefault(A[i],0) + 1);       // Increase frequency for value A[i]
+        for (int i=0; i<A.length; i++){
+            frequency.put(A[i], frequency.getOrDefault(A[i],0) + 1);    // count occurrences
+        }
         
-        for (int i=0;i<A.length;i++)
-        {
-            if (frequency.get(A[i]) > 0) 
-            {
-                int temp = (A[i] >= 0 ? 2*A[i] : A[i]/2);
-                if (frequency.getOrDefault(temp, 0) <= 0)     // If A[i] cannot be matched
-                    return false;
-                else
-                {
-                    // Decrease frequency of A[i] and 2*A[i]
+        for (int i=0; i<A.length; i++){
+            if (frequency.get(A[i]) > 0){
+                int temp = (A[i] > 0 ? 2*A[i] : A[i]/2);    // calculate the paired value
+                if (frequency.getOrDefault(temp,0) == 0){   // if A[i] can't be paired
+                    return false;   
+                }
+                else{
                     frequency.put(A[i], frequency.get(A[i]) - 1);
                     frequency.put(temp, frequency.get(temp) - 1);
                 }
             }
         }
+        
         return true;
     }
 }
