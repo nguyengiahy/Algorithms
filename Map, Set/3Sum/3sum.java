7eee0763f96b587c3cb48fc7255fc679db78c943
Hy Nguyen <nguyengiahy2911@gmail.com>
2020-09-25 22:50:21 +0700

    fix 3sum

--- Map, Set/3Sum/3sum.java
+++ Map, Set/3Sum/3sum.java
@@ -1,30 +1,29 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        if (nums.length < 3) return new ArrayList<>(); // if nums less than 3 element
-        
-        Arrays.sort(nums); // sort array
         Set<List<Integer>> set = new HashSet<>();
-        List resultList=new ArrayList();
+        List result = new ArrayList();
+        Arrays.sort(nums);
         
-        for(int i=0;i<nums.length-1;i++){
-            int start=i+1;
-            int end=nums.length-1;
-            while(start<end){
-                if(nums[i]+ nums[start]+nums[end]==0){
-                    // Hash set will remove duplicates if any
+        for (int i=0; i<nums.length-2; i++){
+            int start = i+1;
+            int end = nums.length-1;
+            while (start < end){
+                if (nums[i] + nums[start] + nums[end] == 0){
+                    // Hash set will remove duplicates
                     set.add(Arrays.asList(nums[i],nums[start],nums[end]));
                     end--;
-                    start ++;
-                }else if(nums[i]+ nums[start]+nums[end]>0){
+                    start++;
+                }
+                else if (nums[i] + nums[start] + nums[end] > 0){
                     end--;
-                }else{
+                }
+                else{
                     start++;
                 }
             }
         }
-
-        //Return by adding into List
-        resultList.addAll(set);
-        return resultList;
+        
+        result.addAll(set);
+        return result;
     }
 }
